eb515ebaf840291586a365d58f2df490e3828d9a
Laplace <mbptemp555@gmail.com>
2021-06-22 07:21:38 -0400

    Stalls out, need to find more clever solution

--- pe700.py
+++ pe700.py
@@ -1,16 +1,75 @@
-inc = 1504170715041707
-md  = 4503599627370517
+from timeme import *
 
-total = 1504170715041707
-mn = total
+def invEuclid(a,b):
+    modulus = a
+    # a > b
+    coeff = []
 
-current = total
+    while b > 0:
+        coeff.append(a // b)
+        a, b = b, a%b
 
-while mn > 0:
-    current = (current + inc) % md
-    if current < mn:
-        total += current
-        print(mn)
-        mn = current
+    # Now inverse
 
-print(total)
+    m, n = (0,1)
+
+    # Skip last coefficient
+    for c in coeff[:-1]:
+        m, n = n, m - c*n
+
+    return n % modulus
+
+def run():
+    inc = 1504170715041707
+    modulus  = 4503599627370517
+
+    inverse = invEuclid(modulus, inc)
+
+    total = inc
+    mn = total
+
+    current = total
+
+    # Loop for initial mining
+    while mn > 10**9:
+        current = (current + inc) % modulus
+
+        if current < mn:
+            # Found a coin!
+            print("Coin:", current)
+            total += current
+
+            diff = mn - current
+
+            while diff < current:
+                current -= diff # Next coin
+                print("Coin:", current)
+                total += current
+
+            mn = current
+
+    # Goal is now "small"
+    while mn > 0:
+        best_n = (mn * inverse) % modulus
+
+        for test in range(mn-1,0,-1):
+            n = (test * inverse) % modulus
+
+            if n > best_n:
+                best_n = n
+
+                current = test
+                diff = mn - current
+
+                while diff < current:
+                    current -= diff # Next coin
+                    print("Coin:", current)
+                    total += current
+
+                mn = current
+                break
+
+    return total
+
+
+timeme(run)
