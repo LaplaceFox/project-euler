76e258ac68db75c17d9b019c0ea485b1bd635267
Laplace <mbptemp555@gmail.com>
2021-12-04 00:35:07 -0500

    Much faster solution for 132

--- pe132.py
+++ pe132.py
@@ -1,55 +1,33 @@
 from primes import nextPrime
 from timeme import timeme
+from math import floor
 
-def repu(k):
-	# Return repunit of length k (for testing)
-	return int("1" * k)
+def bigpowmod(b,e,m):
+	acc = 1
 
-def repuRes(k,d):
-	# Residue of repunit length k divided by d
+	while e > 0:
+		if e % 2 == 1:
+			acc = (acc * b) % m
 
-	if 10 % d == 0:
-		return 1
+		b = b**2 % m
+		e = floor(e/2)
 
-	resloop = [1]
-	cur = 1
+	return acc
 
-	while True:
-		# Get residue of next place value
-		cur = (cur * 10) % d
 
-		if cur == 1: #if already looped
-			break
-		else:
-			resloop.append(cur)
+def run():
+	ct = 0
+	tot = 0
+	p = 7
 
-	looplen = len(resloop)
-
-	# Total for all repeated loops
-	loops_sum = sum(resloop) * (k // looplen)
-
-	# Get sum of residues in partial loop at high end of repunit
-	part_sum = sum(resloop[:(k % looplen)]) % d
-
-	return (part_sum + loops_sum) % d
-
-def run(size,count):
-	p = 2 #current prime
-	ct = 0 #prime divisors found
-	tot = 0 #total of divisors
-
-	while ct < count:
-		if repuRes(size, p) == 0:
-			ct += 1
+	while ct < 40:
+		if bigpowmod(10,10**9,p) == 1:
 			tot += p
+			ct += 1
 			print("Found", p)
 		p = nextPrime(p)
 
 	return tot
 
-
 if __name__ == "__main__":
-	def test():
-		return run(10**9, 40)
-
-	timeme(test)
+	timeme(run)
