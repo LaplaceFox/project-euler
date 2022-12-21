bc00cdcb72c7acecd798c3aa572f665e5da289bd
LaplaceFox <mbptemp555@gmail.com>
2022-12-21 07:42:26 -0700

    Solve 719 by adding extra condition to avoid unnecessary recursive calls

--- pe719.py
+++ pe719.py
@@ -1,6 +1,9 @@
-from time import time
+from timeme import timeme
 
 def splitsum(n, tgt):
+    if n < tgt:
+        return False
+
     #print((n,tgt))
     if n == tgt:
         return True
@@ -19,27 +22,23 @@ def splitsum(n, tgt):
         if left > tgt: # Already got too big
             return False
 
-        if right < tgt-left:
-            return False
-
         if splitsum(right, tgt-left):
             return True
 
     return False
 
-begin = time()
+def solution():
+    total = 0
 
-total = 0
+    for i in range(2,10**6 + 1):
+        if i % 10000 == 0:
+            print("->" + str(i//10000))
 
-for i in range(2,10**6 + 1):
-    if i % 10000 == 0:
-        print("->" + str(i//10000))
+        if splitsum(i**2, i):
+            #print(True)
+            total += i**2
 
-    if splitsum(i**2, i):
-        #print(True)
-        total += i**2
+        #print("------")
+    return total
 
-    #print("------")
-
-print(total)
-print("Time taken:", time()-begin, chr(7))
+timeme(solution)
