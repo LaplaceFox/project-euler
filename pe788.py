53debeaf91ca7af4b42a8414f5fc645e7dd3ecd1
LaplaceFox <mbptemp555@gmail.com>
2023-01-11 09:38:40 -0700

    100x speedup on 788

--- pe788.py
+++ pe788.py
@@ -15,17 +15,25 @@ def fact(n):
 
 def d(k):
     res = 0
-    for i in range(k//2 + 1, k+1):
-        res += 9**(k-i) * fact(k)//(fact(i)*fact(k-i))
+
+    lo = k//2 + 1
+    bin = fact(k)//(fact(lo)*fact(k-lo))
+
+    for i in range(lo, k):
+        res += bin
+        bin = bin * (k-i) // (i+1)
+        res *= 9
+
+    res += 1  # n choose n
     
-    return (res * 9) % MODULUS
+    return res % MODULUS
 
 def dominating(n):
     res = 0
     for i in range(1,n+1):
         res += d(i)
     
-    return res % MODULUS
+    return (res * 9) % MODULUS
 
 def solution():
     return dominating(2022)
