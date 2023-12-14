4282d29d4016179a2de584d1f4aff220a9e194f2
LaplaceFox <mbptemp555@gmail.com>
2023-12-13 21:27:48 -0700

    549 technically under a minute

--- pe549.py
+++ pe549.py
@@ -1,9 +1,13 @@
 from copy import deepcopy as copy
+from math import floor, log
 from primes import megasieve
 from timeme import timeme
 
 # Smallest factorial that has p^k as a factor
 def prime_power_s(p,k):
+    if k == 1 or k == 2:
+        return k*p
+
     n = 0
     num_factors = 1
     res = 0
@@ -46,6 +50,8 @@ def S(n):
         #print(p)
         threshold = n/p  # If a number is above this, don't add to new dict, then add s-value to total
 
+        precompute = [prime_power_s(p,k) for k in range(floor(log(n,p))+1)]
+
         for key in found:
             for num in found[key]:
                 if num > threshold:
@@ -54,11 +60,15 @@ def S(n):
                     continue
 
                 k = 0
-                while num * (p**k) <= n:
-                    new_key = max(key, prime_power_s(p,k))
+                bound = log(n/num, p)
+
+                for b in range(floor(bound)):
+                    new_key = max(key, precompute[k])
                     list_dict_insert(new_dict, new_key, num * (p**k))
 
                     k += 1
+
+                total += max(key, prime_power_s(p,k)) #don't bother adding this one to the dict
 
         found = copy(new_dict)
         new_dict = {}
